8cdee27849d32254a7ce46c803cf1495d2a2504c
ana julia Rufino Ramos <anajuliru@gmail.com>
2023-05-12 15:57:22 -0300

    k

---
+++ codigo/README.md
@@ -0,0 +1,2 @@
+# Codigo das atividades de introdução a linguagem C
+## Nome: Ana Julia Rufino Ramos
--- codigo/ativVetores1.cpp
+++ codigo/ativVetores1.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-int main() {
 
-	cout << "pi";
+int main(void) {
+    int I, A[8], B[8], mult;
 
-		return 0;
+    cout << " Digite os 8 numeros apertando em <ENTER> apos cada um" << endl;
+    for (I = 0; I < 8; I++) {
+        cout << " Digite o numero escolhido " << I + 1 << endl;
+        cin >> A[I];
+        B[I] = (A[I] * 3);
+        
+    }
+
+        for (I = 0; I < 8; I++) {
+            mult = A[I] * B[I];
+            cout << "Valor de " << A[I] << " x " << B[I] << " eh igual a " << mult << endl;
+
+        }
+        
+        return 0;
 }
